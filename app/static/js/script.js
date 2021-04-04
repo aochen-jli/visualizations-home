86b147572eac48f8561eebb913569fab0a626d9c
aochen-jli <aochen.jli@gmail.com>
2021-04-03 18:34:19 -0700

    Added popup window to home page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded", CollapsibleLists.apply);
+$(document).ready(function() {
+    CollapsibleLists.apply();
+    $('#popup').click(togglePopup);
+
+    // Region-clicking and loading icon logic
+    let loader = $('#popup .loader');
+    let iframe = $('#popup iframe');
+    $('.regions a').click(function(e) {
+        e.preventDefault();
+        iframe.attr("src", e.target.href);
+        loader.css("display", "block");
+        togglePopup();
+    });
+    iframe.on("load", function() {
+        loader.css("display", "none");
+    });
+});
+
+function togglePopup() {
+    let popup = $('#popup');
+    if (popup.css("display") === "none") {
+        $('body').css("overflow", "hidden");
+        popup.css("display", "block");
+    } else {
+        $('body').css("overflow", "auto");
+        popup.css("display", "none");
+
+        // Need to unload iframe upon exit, otherwise it will
+        // persist when clicking the next region
+        $('#popup iframe').attr("src", "about:blank");
+    }
+}
